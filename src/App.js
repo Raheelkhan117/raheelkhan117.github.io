6a51cf8a2176000d6d5c9bfb1b332b4ab784846f
Raheel khan <khanraheel755@gmail.com>
2023-05-21 16:58:33 +0500

    commit message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from "react";
+import  axios  from "axios";
+import { MovieCard } from './components/MovieCard/moviecard';
 function App() {
+  const [movies, setMovies ] = useState([]);
+  const getMovies = async() => {
+try{
+const { data } = await axios.get("https://movies-app.prakashsakari.repl.co/api/movies");
+setMovies(data);
+}
+catch(err){
+  console.log(err);
+}
+}
+useEffect(() => {
+getMovies();
+},[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     <header className='header'><h1 className='heading_1'>Movies</h1>
+     </header>
+     <main className='main'>{ movies && movies.length>0 && movies.map(movie => <MovieCard key = {movie.id} movie ={movie} />)
+      }</main>
+     
     </div>
   );
 }
