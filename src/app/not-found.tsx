b7811edff5f731eb074b0751705c4cd762afd941
Raheel Khan <khanraheel755@gmail.com>
2025-04-26 10:54:30 +0500

    Initial commit

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+    return (
+      <div className="min-h-screen flex flex-col items-center justify-center text-white dark:bg-neutral-900 px-4">
+        <h1 className="text-4xl font-bold font-mono mb-2">404 - Not Found</h1>
+        <p className="text-neutral-400">The page you are looking for doesn’t exist.</p>
+      </div>
+    )
+  }
+  
